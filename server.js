3395edcac843858ee63719107456271327799141
Abhay Singh Chauhan <75360601+arcaneAB@users.noreply.github.com>
2021-09-16 00:19:51 +0530

    final commit

--- server.js
+++ server.js
@@ -1,32 +1,42 @@
-const { on } = require("events");
-const express = require("express");
-const { join } = require("path");
-const app = express();
-const server = require("http").Server(app);
+const express = require('express')
+const app = express()
+// const cors = require('cors')
+// app.use(cors())
+const server = require('http').Server(app)
 const io = require('socket.io')(server)
-const { v4: uuidv4} = require('uuid');
-const {ExpressPeerServer}=require('peer');
-const peerServer = ExpressPeerServer(server,{
-    debug: true
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+  debug: true
 });
+const { v4: uuidV4 } = require('uuid')
 
-app.set('view engine','ejs');
-app.use(express.static('public'));
+app.use('/peerjs', peerServer);
 
-app.use('/peerjs',peerServer); 
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
-app.get("/",(req,res)=>{
-    res.redirect(`/${uuidv4()}`);
+app.get('/', (req, res) => {
+  res.redirect(`/${uuidV4()}`)
 })
 
-app.get("/:room",(req,res)=>{
-    res.render('room',{roomId: req.params.room});
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room })
 })
 
-io.on('connection',socket=>{
-    socket.on('join-room',(roomId,userId)=>{
-        socket.join(roomId);
-        socket.to(roomId).emit('user-connected',userId);
+io.on('connection', socket => {
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId)
+    socket.to(roomId).broadcast.emit('user-connected', userId);
+    // messages
+    socket.on('message', (message) => {
+      //send message to the same room
+      io.to(roomId).emit('createMessage', message)
+  }); 
+
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('user-disconnected', userId)
     })
+  })
 })
-server.listen(3030);
+
+server.listen(process.env.PORT||3030)
